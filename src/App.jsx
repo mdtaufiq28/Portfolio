3f23e35b18439fc3e648a1e0ad8976d62985dc0a
mdtaufiq28 <mohammed.taufiq.n@gmail.com>
2025-02-13 15:46:28 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Route,Routes } from 'react-router-dom';
-import Layout from '../pages/layout';
+import Layout from '../pages/Layout';
 import Home from '../pages/Home';
 import AboutMe from '../pages/AboutMe';
 import Projects from '../pages/Projects';
