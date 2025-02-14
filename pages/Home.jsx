2972fbfd33b518f892ce452f289ae8128ee67b67
mdtaufiq28 <mohammed.taufiq.n@gmail.com>
2025-02-14 20:19:40 +0530

    commit 6

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='pl-20 flex flex-col items-start gap-y-7 border-2'>
+    <div className='pl-20 flex flex-col items-start gap-y-7 '>
         <p className='text-7xl font-bold '>Muhammad Taufiq</p>
 
         <p className='ml-1 text-2xl'>Front-End Web Developer | ReactJS HTML CSS Javascript </p>
