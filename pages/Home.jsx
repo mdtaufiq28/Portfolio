6ad945ef0e37ac196b2852e4ac39ff7a33deaf63
mdtaufiq28 <mohammed.taufiq.n@gmail.com>
2025-02-14 20:16:37 +0530

    commit 3

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,17 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='pl-20 flex flex-col items-center gap-y-7 '>
+    <div className='pl-20 flex flex-col items-start gap-y-7 '>
         <p className='text-7xl font-bold '>Muhammad Taufiq</p>
 
-        <p className='ml-1 text-2xl'>Front-End Web Developer | ReactJS HTML CSS Javascript</p>
+        <p className='ml-1 text-2xl'>Front-End Web Developer | ReactJS HTML CSS Javascript </p>
         
-        <div className='flex '>
         <a href="https://drive.google.com/uc?export=download&id=1tdjvZndJvjUok98fD5Zh8nssjcyItdNo"  download={"Muhammad Taufiq - Resume"} className='ml-1 bg-cyan-700 text-white p-6 rounded-full cursor-pointer'>
-          Download Resume
+        Download Resume
         </a>
-
-      </div>
+        
     </div>
   )
 }
